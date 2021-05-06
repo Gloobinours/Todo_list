3e1f83c7d1746a02956517303a814f06cd48b0ca
Gloobinours <kusami.louan@gmail.com>
2021-05-06 09:26:46 -0400

    refacto

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
-var tasks = [
+var lists = [
     // {
     //     id: 1,
-    //     text: 'Faire le menage',
-    //     subTasks: [
+    //     title: 'Faire le menage',
+    //     tasks: [
     //         {
     //             id: 2,
     //             text: 'Attrape le balais'
@@ -15,35 +15,74 @@ var textInput = document.getElementById("textInput");
 textInput.focus(); // Gets the focus when the page is loaded
 var id = 0;
 
-const addLine = () => {
-    var text = textInput.value; // gets the value
-    textInput.value = '';
-    textInput.focus(); // gets the focus when the function addLine is called
-    if (text != '') { // If text is empty, 
+// const addLine = () => {
+//     var text = textInput.value; // gets the value
+//     textInput.value = '';
+//     textInput.focus(); // gets the focus when the function addLine is called
+//     if (text != '') { // If text is empty, 
+//         var li = document.createElement('li');
+//         var button = document.createElement('button');
+
+//         var output = document.createTextNode(text);
+//         var buttonText = document.createTextNode('x');
+
+//         li.appendChild(output);
+//         button.appendChild(buttonText);
+//         li.appendChild(button);
+//         document.getElementById('table').appendChild(li);
+
+//         li.setAttribute('key', id);
+//         tasks.push({
+//             text: text,
+//             id: id,
+//             subTasks: []
+//         });
+//         id += 1;
+
+//         button.addEventListener('click', e => { // Removes line when cross is clicked
+//             e.target.parentElement.remove();
+//             var targetId = e.target.parentElement.getAttribute('key');
+//             var targetIndex = tasks.findIndex(task => task.id == targetId);
+//             tasks.splice(targetIndex, 1);
+//             textInput.focus();
+//         });
+
+//     }
+// };
+
+const addList = () => {
+    
+    var title = textInput.value; // gets the value
+    textInput.value = ''; // Resets the input
+    if (title != '') { // If title is not empty, 
         var li = document.createElement('li');
         var button = document.createElement('button');
-
-        var output = document.createTextNode(text);
+        var a = document.createElement('a');
+        var titleText = document.createTextNode(title);
         var buttonText = document.createTextNode('x');
 
-        li.appendChild(output);
+        a.appendChild(titleText)
+        li.addEventListener('click', e => {
+            document.getElementById('modal').style.display = 'block';
+        });
+        li.appendChild(a);
         button.appendChild(buttonText);
         li.appendChild(button);
         document.getElementById('table').appendChild(li);
 
         li.setAttribute('key', id);
-        tasks.push({
-            text: text,
-            id: id,
-            subTasks: []
+        lists.push({
+            id,
+            title,
+            tasks: []
         });
         id += 1;
 
-        button.addEventListener('click', e => {
+        button.addEventListener('click', e => { // Removes line when cross is clicked
             e.target.parentElement.remove();
             var targetId = e.target.parentElement.getAttribute('key');
-            var targetIndex = tasks.findIndex(task => task.id == targetId);
-            tasks.splice(targetIndex, 1);
+            var targetIndex = lists.findIndex(task => task.id == targetId);
+            lists.splice(targetIndex, 1); // removes the elements from lists
             textInput.focus();
         });
 
@@ -51,8 +90,8 @@ const addLine = () => {
 };
 
 // Executes function addLine when key enter is pressed
-document.addEventListener('keydown', (e) => {
+textInput.addEventListener('keydown', (e) => {
     if (e.code === 'Enter') {
-        addLine();
+        addList();
     }
 });
